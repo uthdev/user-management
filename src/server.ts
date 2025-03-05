f244726d552126af2604e49ed38f0d1f3d000ba2
uthdev <adelekegbolahan92@yahoo.com>
2025-03-05 10:52:22 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import  cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
